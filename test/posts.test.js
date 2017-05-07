a82a9e2943e5a22a02872c6e230617857be3f6f4
VAdims Samsinovs <vsamsinovs@gmail.com>
2017-05-07 15:01:50 +0100

    final

--- test/posts.test.js
+++ test/posts.test.js
@@ -1,24 +1,88 @@
 require('babel-polyfill')
 
-import supertest from "supertest";
-import { app } from "./../server";
-import should from "should";
+var supertest = require("supertest");
+var app = require("./../server");
+var should = require("should");
 
 
-// UNIT test begin
-describe("Contacts API unit test", function () {
-  this.timeout(120000);//increase timeout of tests to 2 mins. Starting Mockgoose can take time.
-  // #1 return a collection of json documents
+
+describe("posts API unit test", function () {
+  this.timeout(120000);
+
   it("should return collection of JSON documents", function (done) {
-    // calling home page api
-    supertest(app)
-      .get("/api/contacts")
+    supertest(server)
+      .get("/api/posts")
       .expect("Content-type", /json/)
-      .expect(200) // This is the HTTP response
+      .expect(200)
       .end(function (err, res) {
-        // HTTP status should be 200
         res.status.should.equal(200);
         done();
       });
   });
+
+  it("should add a post", function (done) {
+    supertest(server)
+      .post('/api/posts')
+      .send({ name: "post 99", address: "123 Strand St" })
+      .expect("Content-type", /json/)
+      .expect(201)
+      .end(function (err, res) {
+        res.status.should.equal(201);
+        res.body.should.have.property('_id');
+        done();
+      });
+  });
+
+  it("should delete post", function (done) {
+    const superserver = supertest(server);
+    superserver
+      .get("/api/posts")
+      .expect("Content-type", /json/)
+      .expect(200)
+      .end(function (err, res) {
+        const id = res.body[0]._id;
+        superserver
+          .delete("/api/posts/" + id)
+          .expect("Content-type", /json/)
+          .expect(200)
+          .end(function (err, res) {
+            res.body._id.should.equal(id);
+            done();
+          }
+          );
+      }
+      );
+  });
+
+
+  it("should update post", function (done) {
+    const superserver = supertest(server);
+    superserver
+      .get("/api/posts")
+      .expect("Content-type", /json/)
+      .expect(200) // THis is HTTP response
+      .end(function (err, res) {
+
+        const id = res.body[0]._id,
+          name = res.body[0].name,
+          address = res.body[0].address;
+
+        superserver
+          .put("/api/posts/" + id)
+          .send({ name: name + 1, address: address + 1 })
+          .expect("Content-type", /json/)
+          .expect(200) // THis is HTTP response
+          .end(function (err, res) {
+
+            should.exist(res.body);
+            res.status.should.equal(200);
+
+            res.body.should.have.property('_id');
+            res.body.name.should.equal(name + 1);
+            res.body.address.should.equal(address + 1);
+            done();
+          });
+      });
+  });
 });
+
